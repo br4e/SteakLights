bab76ffbeb8aa694c894f12871c451da1da06a56
br4e <br4e@protonmail.com>
2022-08-23 22:26:22 -0700

    Added sparkles!

--- lil_tenderloin/lil_tenderloin.ino
+++ lil_tenderloin/lil_tenderloin.ino
@@ -24,44 +24,37 @@
 //      delay(25);
 //  }
 //}
-​
-​
-​
-​
-​
-​
+
+
+
 #include <Adafruit_NeoPixel.h>
 #define LED_PIN1 6
 #define LED_COUNT1 92
 #define LED_PIN2 7
 #define LED_COUNT2 21
-​
+
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(LED_COUNT1, LED_PIN1, NEO_RGBW + NEO_KHZ800);
 Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(LED_COUNT2, LED_PIN2, NEO_RGBW + NEO_KHZ800);
+
+// put your setup code here, to run once:
 void setup() {
-  // put your setup code here, to run once:
   strip1.begin();
   strip1.show(); // Initialize all pixels to 'off'
   strip2.begin();
   strip2.show(); // Initialize all pixels to 'off'
 }
-​
+
 void loop() {
   // put your main code here, to run repeatedly:
-  strip1.fill(strip1.Color(0,225,0), 0, LED_COUNT1);
+  strip1.fill(strip1.Color(0,255,0), 0, LED_COUNT1);
+  strip2.fill(strip2.Color(0,0,0,255), 0, LED_COUNT2);
+  
+  for (long i = 0; i < 10; i++) {
+    strip1.setPixelColor(random(0, LED_COUNT1), 0, 0, 0, 255);
+  }
+  
   strip1.show();
-  strip2.fill(strip2.Color(0,0,0,225), 0, LED_COUNT2);
   strip2.show();
-  delay(25);
+  
+  delay(50);
 }
-​
-​
-//void loop() {
-//  // put your main code here, to run repeatedly:
-//  #define ACTIVE_PIN 0;
-//  //strip.fill(strip.Color(255, 0, 0, 0), 0, LED_COUNT);
-//void loop() {
-//  strip.setPixelColor(ACTIVE_PIN, 255, 0, 0, 0);
-//  ACTIVE_PIN = ACTIVE_PIN + 1;
-//}
-//}
